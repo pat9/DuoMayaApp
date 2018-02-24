bfad89724c29edb5a3ec8336dc881490d0431e83
Daniel Pat <danielpat6@gmail.com>
2018-02-24 15:39:55 -0600

    Login y Registro

--- app/src/main/java/com/example/luisr/duomayaapp/RegistroActivity.java
+++ app/src/main/java/com/example/luisr/duomayaapp/RegistroActivity.java
@@ -2,14 +2,55 @@ package com.example.luisr.duomayaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegistroActivity extends AppCompatActivity {
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import AsyncTasks.descargarDatosAsyncTask;
+public class RegistroActivity extends AppCompatActivity implements descargarDatosAsyncTask.interfacedelhilo {
+    EditText txtUsuario, txtPassword, txtEmail, txtNombre, txtApellido;
+    Button btnEntrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        txtPassword = findViewById(R.id.txtPassword);
+        txtApellido = findViewById(R.id.txtApellido);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtUsuario = findViewById(R.id.txtUsuario);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                descargarDatosAsyncTask obj = new descargarDatosAsyncTask();
+                obj.delegado = RegistroActivity.this;
+                try
+                {
+                    obj.execute(new URL("http://aprendermayaws.gearhostpreview.com/AprenderMayaWS.asmx/RegistroUsuario?NickName="+txtUsuario.getText().toString()+"&Password="+txtPassword.getText().toString()+"&FotoPerfil=Foto"+"&Correo="+txtEmail.getText().toString()+"&Nombre="+txtNombre.getText().toString()+"&Apellido="+txtApellido.getText().toString()+""));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
 
+    }
+
+    @Override
+    public void datosDescagados(String Datos) {
+        if(Integer.parseInt(Datos) == 1)
+        {
+            Toast.makeText(this,     "Registro completo", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/luisr/duomayaapp/actionLoginActivity.java
+++ app/src/main/java/com/example/luisr/duomayaapp/actionLoginActivity.java
@@ -1,13 +1,65 @@
 package com.example.luisr.duomayaapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class actionLoginActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import AsyncTasks.descargarDatosAsyncTask;
+
+public class actionLoginActivity extends AppCompatActivity implements descargarDatosAsyncTask.interfacedelhilo {
+    EditText txtUsuario, txtPassword;
+    Button btnEntrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_login);
+
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                descargarDatosAsyncTask obj = new descargarDatosAsyncTask();
+                obj.delegado = actionLoginActivity.this;
+                try
+                {
+                    obj.execute(new URL("http://aprendermayaws.gear.host/AprenderMayaWS.asmx/Login?Usuario="+txtUsuario.getText().toString()+"&Password="+txtPassword.getText().toString()));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public void datosDescagados(String Datos) {
+        try
+        {
+            JSONObject Objeto = new JSONObject(Datos);
+            if(Objeto.getString("NickName") == "null" )
+            {
+                Toast.makeText(this, "Error en el login", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Bienvenido " + Objeto.getString("NickName"), Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error en el login", Toast.LENGTH_SHORT).show();
+        }
     }
 }
