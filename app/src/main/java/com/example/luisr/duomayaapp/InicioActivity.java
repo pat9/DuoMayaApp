4837cc6e29efae78ee2922cfe558bd8a53165ae6
Wisho <luisrhxq@hotmail.com>
2018-02-25 21:48:51 -0600

    DrawerMenu Added

--- app/src/main/java/com/example/luisr/duomayaapp/InicioActivity.java
+++ app/src/main/java/com/example/luisr/duomayaapp/InicioActivity.java
@@ -3,16 +3,27 @@ package com.example.luisr.duomayaapp;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 public class InicioActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        drawerLayout=(DrawerLayout) findViewById(R.id.DrawerId);
+        actionBarDrawerToggle= new ActionBarDrawerToggle(this,drawerLayout,R.string.Open,R.string.Close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         BottomNavigationView bottomNav = findViewById(R.id.Id_BottomNavigation);
         bottomNav.setOnNavigationItemSelectedListener(NavListener);
@@ -22,6 +33,14 @@ public class InicioActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().replace(R.id.Fragment_ContainerXML,new FragmentPerfilClass()).commit();
         }
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener NavListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
